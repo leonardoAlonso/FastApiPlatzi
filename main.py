510d0109a337e92ef0c73838404736b2ec2b3e5e
Leonardo Alonso <leonardoalonsososa@gmail.com>
2021-11-18 18:02:20 -0600

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# Fast API
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+# Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get('/')
 def home():
     return {'helo': 'world'}
+
+# Request and response body
+@app.post('/person/new')
+def create_person(person: Person = Body(...)):
+    return person
