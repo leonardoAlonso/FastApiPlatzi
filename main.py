51fa7a5ba016e016d677a0cf74166a68a71bb715
Leonardo Alonso <leonardoalonsososa@gmail.com>
2021-12-23 17:09:00 -0600

    summary

--- main.py
+++ main.py
@@ -90,9 +90,21 @@ def home():
     path='/person/new',
     response_model=PersonOut,
     status_code=status.HTTP_201_CREATED,
-    tags=['Persons']
+    tags=['Persons'],
+    summary="Create person in the app"
 )
 def create_person(person: Person = Body(...)):
+    """
+    Create Person
+
+    This path operation created a person in the app and save the information in the database
+
+    Parameters:
+    - Request body parameter:
+        - **person: Person** -> A person model with First Name, Last Name, Age, Hair Color and Marital status
+
+    Returns a person model with first name, last name, age, hair color and marital status
+    """
     return person
 
 # Validaciones: Query parameters
